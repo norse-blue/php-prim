eaae997f9d5bbe514dcb9c41c148498919bbf1d8
Axel Pardemann <axel.pardemann@norse.blue>
2019-05-04 11:30:28 -0500

    Refactor StringIsExtension
    # Explain why this change is being made
    
    # Provide links to any relevant tickets, articles or other resources

--- src/Scalars/Extensions/String/StringIsExtension.php
+++ src/Scalars/Extensions/String/StringIsExtension.php
@@ -33,8 +33,6 @@ class StringIsExtension extends StringObject implements ExtensionMethod
             }
 
             foreach ($patterns as $pattern) {
-                $pattern = self::unwrap($pattern);
-
                 // If the given value is an exact match we can of course return true right
                 // from the beginning. Otherwise, we will translate asterisks and do an
                 // actual pattern match against the two strings to see if they match.
@@ -42,12 +40,12 @@ class StringIsExtension extends StringObject implements ExtensionMethod
                     return bool(true);
                 }
 
-                $pattern = string(preg_quote($pattern, '#'));
-
                 // Asterisks are translated into zero-or-more regular expression wildcards
                 // to make it convenient to check if the strings starts with the given
                 // pattern such as "library/*", making any string check convenient.
-                $pattern = $pattern->replace('\*', '.*');
+                $pattern = string($pattern)
+                    ->regexQuote('#')
+                    ->replace('\*', '.*');
                 if (count($this->regexMatches('#^' . $pattern . '\z#u')) > 0) {
                     return bool(true);
                 }
