8ee90e432c81b6eb8b4db6dd0580405e05f50b96
Axel Pardemann <axel.pardemann@norse.blue>
2019-05-20 17:24:00 -0500

    Add Arr::each extension method
    # Explain why this change is being made
    
    # Provide links to any relevant tickets, articles or other resources

---
+++ src/Collections/Extensions/Arr/ArrEachExtension.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace NorseBlue\Prim\Collections\Extensions\Arr;
+
+use NorseBlue\ExtensibleObjects\Contracts\ExtensionMethod;
+use NorseBlue\Prim\Collections\ArrObject;
+
+/**
+ * Class ArrEachExtension
+ *
+ * @package NorseBlue\Prim\Collections\Extensions\Arr
+ */
+class ArrEachExtension extends ArrObject implements ExtensionMethod
+{
+    /**
+     * @return callable(callable($item, $key) $callback): Arr
+     */
+    public function __invoke(): callable
+    {
+        /**
+         * Execute a callback over each item.
+         *
+         * @param callable $callback
+         *
+         * @return \NorseBlue\Prim\Collections\Extensions\Arr\ArrEachExtension
+         */
+        return function (callable $callback): ArrObject {
+            foreach ($this->items as $key => $item) {
+                if ($callback($item, $key) === false) {
+                    break;
+                }
+            }
+
+            return $this;
+        };
+    }
+}
---
+++ tests/Unit/Collections/Arr/ArrEachTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace NorseBlue\Prim\Tests\Unit\Collections\Arr;
+
+use NorseBlue\Prim\Collections\ArrObject;
+use NorseBlue\Prim\Tests\TestCase;
+
+/**
+ * Class ArrEachTest
+ *
+ * @package NorseBlue\Prim\Tests\Unit\Collections\Arr
+ */
+class ArrEachTest extends TestCase
+{
+    /** @test */
+    public function arr_each()
+    {
+        $arr = new ArrObject($original = [1, 2, 'foo' => 'bar', 'bam' => 'baz']);
+
+        $result = [];
+        $arr_result = $arr->each(function ($item, $key) use (&$result) {
+            $result[$key] = $item;
+        });
+        $this->assertEquals($original, $result);
+        $this->assertInstanceOf(ArrObject::class, $arr_result);
+        $this->assertSame($arr, $arr_result);
+
+        $result = [];
+        $arr_result = $arr->each(function ($item, $key) use (&$result) {
+            $result[$key] = $item;
+            if (is_string($key)) {
+                return false;
+            }
+        });
+        $this->assertEquals([1, 2, 'foo' => 'bar'], $result);
+        $this->assertInstanceOf(ArrObject::class, $arr_result);
+        $this->assertSame($arr, $arr_result);
+    }
+}
