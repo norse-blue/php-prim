8436e39c3d2bb9eb53aa5b109897fa53baaa7557
Axel Pardemann <axel.pardemann@norse.blue>
2019-05-25 15:51:09 -0500

    Fix issue where ArrObject::average tests where not running
    # Explain why this change is being made
    
    # Provide links to any relevant tickets, articles or other resources

---
+++ src/Facades/Facade.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace NorseBlue\Prim\Facades;
+
+use BadMethodCallException;
+use NorseBlue\Prim\Exceptions\InvalidFacadeClassException;
+use NorseBlue\Prim\ValueObject;
+
+abstract class Facade
+{
+    /** @var string The class that this facade is for. */
+    protected static $class = '';
+
+    /** @var array The name of the methods that are actually static. */
+    protected static $statics = [
+        'unwrap',
+    ];
+
+    /**
+     * ValueObjectFacade constructor.
+     *
+     * @codeCoverageIgnore
+     */
+    final private function __construct()
+    {
+    }
+
+    // region === Magic Methods ===
+
+    /**
+     * @inheritDoc
+     */
+    final public static function __callStatic(string $method, array $arguments)
+    {
+        $class = static::$class;
+        self::validateFacadeClass($class);
+        self::validateFacadeMethod($class, $method);
+
+        if (in_array($method, array_merge(self::$statics, static::$statics), true)) {
+            return $class::$method(...$arguments);
+        }
+
+        return (new $class(array_shift($arguments)))->$method(...$arguments);
+    }
+
+    // endregion Magic Methods
+
+    /**
+     * Validate the facade class.
+     *
+     * @param string $class
+     *
+     * @return void
+     * @throws \NorseBlue\Prim\Exceptions\InvalidFacadeClassException
+     */
+    final protected static function validateFacadeClass(string $class): void
+    {
+        if (!is_string($class) || $class === '' || !class_exists($class)) {
+            throw new InvalidFacadeClassException('A valid facade class has not been set.');
+        }
+
+        static::validateFacadeClassType($class);
+    }
+
+    /**
+     * Validate the facade class type.
+     *
+     * @param string $class
+     *
+     * @return void
+     */
+    abstract protected static function validateFacadeClassType(string $class): void;
+
+    /**
+     * Validate the facade method.
+     *
+     * @param string $class
+     * @param string $method
+     */
+    final protected static function validateFacadeMethod(string $class, string $method): void
+    {
+        /** @var ValueObject $class */
+        if (!method_exists($class, $method) && !$class::hasExtensionMethod($method)) {
+            throw new BadMethodCallException("The method $method does not exist for class $class.");
+        }
+    }
+}
--- src/Facades/ValueObjectFacade.php
+++ src/Facades/ValueObjectFacade.php
@@ -2,7 +2,6 @@
 
 namespace NorseBlue\Prim\Facades;
 
-use BadMethodCallException;
 use NorseBlue\Prim\Exceptions\InvalidFacadeClassException;
 use NorseBlue\Prim\ValueObject;
 
@@ -13,76 +12,20 @@ use NorseBlue\Prim\ValueObject;
  *
  * @method static mixed unwrap(mixed $value)
  */
-abstract class ValueObjectFacade
+abstract class ValueObjectFacade extends Facade
 {
     /** @var string The class that this facade is for. */
     protected static $class = ValueObject::class;
 
-    /** @var array The name of the methods that are actually static. */
-    protected static $statics = [
-        'unwrap',
-    ];
-
-    /**
-     * ValueObjectFacade constructor.
-     *
-     * @codeCoverageIgnore
-     */
-    final private function __construct()
-    {
-    }
-
-    // region === Magic Methods ===
-
     /**
      * @inheritDoc
      */
-    final public static function __callStatic(string $method, array $arguments)
+    final protected static function validateFacadeClassType(string $class): void
     {
-        $class = static::$class;
-        self::validateFacadeClass($class);
-        self::validateFacadeMethod($class, $method);
-
-        if (in_array($method, array_merge(self::$statics, static::$statics), true)) {
-            return $class::$method(...$arguments);
-        }
-
-        return (new $class(array_shift($arguments)))->$method(...$arguments);
-    }
-
-    // endregion Magic Methods
-
-    /**
-     * Validate the facade class.
-     *
-     * @param string $class
-     *
-     * @throws \NorseBlue\Prim\Exceptions\InvalidFacadeClassException
-     */
-    final protected static function validateFacadeClass(string $class): void
-    {
-        if (!is_string($class) || $class === '' || !class_exists($class)) {
-            throw new InvalidFacadeClassException('A valid facade class has not been set.');
-        }
-
-        if (!is_subclass_of($class, ValueObject::class)) {
+        if (!is_subclass_of($class, self::$class)) {
             throw new InvalidFacadeClassException(
-                sprintf('The class %s does not extend class %s.', $class, ValueObject::class)
+                sprintf('The class %s does not extend class %s.', $class, static::$class)
             );
-        }
-    }
-
-    /**
-     * Validate the facade method.
-     *
-     * @param string $class
-     * @param string $method
-     */
-    final protected static function validateFacadeMethod(string $class, string $method): void
-    {
-        /** @var ValueObject $class */
-        if (!method_exists($class, $method) && !$class::hasExtensionMethod($method)) {
-            throw new BadMethodCallException("The method $method does not exist for class $class.");
         }
     }
 }
