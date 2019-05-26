33023ee75b6b8afe30b55d6712d94ca7b647e686
Axel Pardemann <axel.pardemann@norse.blue>
2019-05-26 09:29:48 -0500

    Add ArrObject::contains extension method
    # Explain why this change is being made
    
    # Provide links to any relevant tickets, articles or other resources

--- src/Collections/ArrObject.php
+++ src/Collections/ArrObject.php
@@ -6,6 +6,7 @@ namespace NorseBlue\Prim\Collections;
 
 use ArrayAccess;
 use NorseBlue\Prim\Contracts\Jsonable;
+use NorseBlue\Prim\Scalars\BoolObject;
 use NorseBlue\Prim\Scalars\IntObject;
 use NorseBlue\Prim\Traits\ContainerArrayAccess;
 
@@ -15,6 +16,7 @@ use NorseBlue\Prim\Traits\ContainerArrayAccess;
  * @package NorseBlue\Prim\Collections
  *
  * @method IntObject average(int|IntObject $decimals = 0)
+ * @method BoolObject contains(mixed $needle, bool|BoolObject $strict = false)
  * @method self each(callable $callback)
  */
 class ArrObject extends ItemContainer implements ArrayAccess, Jsonable
