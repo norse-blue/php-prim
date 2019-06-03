04afd8cbd681bcec74641b85e35ad80bfda4e6bc
Axel Pardemann <axel.pardemann@norse.blue>
2019-06-03 15:44:18 -0500

    Refactor bootstrap
    # Explain why this change is being made
    
    # Provide links to any relevant tickets, articles or other resources

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,6 +4,14 @@ declare(strict_types=1);
 
 namespace NorseBlue\Prim;
 
+use Doctrine\Common\Annotations\Annotation\Enum;
+use NorseBlue\ExtensibleObjects\Contracts\Extensible;
+use NorseBlue\Prim\Types\Collections\ArrObject;
+use NorseBlue\Prim\Types\Scalars\BoolObject;
+use NorseBlue\Prim\Types\Scalars\FloatObject;
+use NorseBlue\Prim\Types\Scalars\IntObject;
+use NorseBlue\Prim\Types\Scalars\NumericObject;
+use NorseBlue\Prim\Types\Scalars\StringObject;
 use Symfony\Component\Finder\Finder;
 use function NorseBlue\Prim\Functions\path_merge;
 
@@ -15,21 +23,25 @@ use function NorseBlue\Prim\Functions\path_merge;
     require_once __DIR__ . DIRECTORY_SEPARATOR . 'Functions' . DIRECTORY_SEPARATOR . 'collections.php';
     require_once __DIR__ . DIRECTORY_SEPARATOR . 'Functions' . DIRECTORY_SEPARATOR . 'scalars.php';
 
-    $types_root = path_merge(__DIR__, 'Types', DIRECTORY_SEPARATOR, true);
-    $extensions_root = path_merge(__DIR__, 'Extensions', DIRECTORY_SEPARATOR, true);
-    $paths = array_keys(iterator_to_array(
-        Finder::create()
-            ->in(path_merge($types_root, '**'))
-            ->name('*Object.php')
-            ->files()
-    ));
+    $extensible_classes = [
+        ArrObject::class,
+        BoolObject::class,
+        Enum::class,
+        FloatObject::class,
+        IntObject::class,
+        NumericObject::class,
+        StringObject::class,
+    ];
 
-    foreach ($paths as $path) {
-        [$category, $type] = explode(DIRECTORY_SEPARATOR, str_replace($types_root, '', $path), 2);
-        $type = preg_replace('%(.+)Object.php$%', '\1', $type);
-        $class = path_merge(Prim::NAMESPACE_ROOT, ['Types', $category, "{$type}Object"], '\\');
-        $extensions_path = path_merge($extensions_root, [$category, $type]);
+    /** @var Extensible $class */
+    foreach ($extensible_classes as $class) {
+        $pattern = '%^NorseBlue\\\\Prim\\\\Types\\\\(?<category>\w+)\\\\(?<type>\w+?)(?:Object)?$%';
+        if (!preg_match($pattern, $class, $data)) {
+            continue;
+        }
 
+        [$category, $type] = [$data['category'], $data['type']];
+        $extensions_path = path_merge(__DIR__, ['Extensions', $category, $type]);
         if (!is_dir($extensions_path)) {
             continue;
         }
@@ -37,22 +49,21 @@ use function NorseBlue\Prim\Functions\path_merge;
         $extensions = array_keys(iterator_to_array(
             Finder::create()
                 ->in($extensions_path)
-                ->name("$type*.php")
+                ->name("{$type}*Extension.php")
                 ->files()
         ));
 
-        foreach ($extensions as $extension) {
-            $pattern = path_merge("%^$extensions_root$category", [$type, $type]) . "(.*)Extension\.php$%";
-            $extension_name = preg_replace($pattern, '\1', $extension);
-
-            $extension_class = path_merge(Prim::NAMESPACE_ROOT, [
+        foreach ($extensions as $path) {
+            $pattern = '%^' . path_merge($extensions_path, $type) . '(.+)Extension\.php$%';
+            $name = preg_replace($pattern, '\1', $path);
+            $extension = path_merge('NorseBlue\Prim', [
                 'Extensions',
                 $category,
                 $type,
-                "{$type}{$extension_name}Extension",
+                "{$type}{$name}Extension",
             ], '\\');
 
-            $class::registerExtensionMethod(lcfirst($extension_name), $extension_class);
+            $class::registerExtensionMethod(lcfirst($name), $extension);
         }
     }
 })();
