601603db9bd21fb7832d6d31d19fda22ca161de2
Axel Pardemann <axel.pardemann@norse.blue>
2019-06-04 17:18:52 -0500

    Refactor Prim
    # Explain why this change is being made
    
    # Provide links to any relevant tickets, articles or other resources

--- src/Prim.php
+++ src/Prim.php
@@ -4,13 +4,7 @@ declare(strict_types=1);
 
 namespace NorseBlue\Prim;
 
-/**
- * Class Prim
- *
- * @package NorseBlue\Prim
- */
 class Prim
 {
     public const VERSION = '0.3.0';
-    public const NAMESPACE_ROOT = 'NorseBlue\Prim';
 }
